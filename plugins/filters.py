06a785a8e213e580cfdd64217ac62b164eef0458
Kopaing12 <107101116+Kopaing12@users.noreply.github.com>
2022-07-22 21:04:02 +0630

    Update filters.py

--- plugins/filters.py
+++ plugins/filters.py
@@ -335,7 +335,7 @@ async def give_filter(client,message):
                     if fileid == "None":
                         if btn == "[]":
                             await message.reply_text(
-                    f"Hello {message.from_user.mention}\n\nသင်ရှာတာ 👉🏻 {message.text}👈🏻  ကို ကျနော်ရှာတွေ့တာပြပေးထားပါတယ်ခင်ဗျာ ☺️ ......\n\n<b>Request by :{message.from_user.mention}\n\n{reply_text}", disable_web_page_preview=True)
+                    f" {photo"https://telegra.ph/file/24785052b59f814dfce7e.png"},Hello {message.from_user.mention}\n\nသင်ရှာတာ 👉🏻 {message.text}👈🏻  ကို ကျနော်ရှာတွေ့တာပြပေးထားပါတယ်ခင်ဗျာ ☺️ ......\n\n<b>Request by :{message.from_user.mention}\n\n{reply_text}", disable_web_page_preview=True)
                         else:
                             button = eval(btn)
                             await message.reply_text(
